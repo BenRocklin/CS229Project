8ad3e6db44a76ea5dd7be6708a3fa38c5b60bdc2
Jacob Meisel <47841665+jacobmeisel@users.noreply.github.com>
2019-11-19 23:13:45 -0800

    push all changes from milestone

--- main.py
+++ main.py
@@ -16,17 +16,17 @@ from tensorflow.keras.models import load_model
 def collectDatasets():
     util.extract_dataset_to_file(saveName="dataSets/two_notes__no_octave.npz",    num_notes=2, use_octave=False, songPath="./songs")
     util.extract_dataset_to_file(saveName="dataSets/three_notes__no_octave.npz",  num_notes=3, use_octave=False, songPath="./songs")
-    util.extract_dataset_to_file(saveName="dataSets/four_notes__no_octave.npz",   num_notes=3, use_octave=False, songPath="./songs")
+    util.extract_dataset_to_file(saveName="dataSets/four_notes__no_octave.npz",   num_notes=4, use_octave=False, songPath="./songs")
     # util.extract_dataset_to_file(saveName="dataSets/two_notes__use_octave.npz",   num_notes=2, use_octave=True,  songPath="./songs")
     # util.extract_dataset_to_file(saveName="dataSets/three_notes__use_octave.npz", num_notes=3, use_octave=True,  songPath="./songs")
-    # util.extract_dataset_to_file(saveName="dataSets/four_notes__use_octave.npz",  num_notes=3, use_octave=True,  songPath="./songs")
+    # util.extract_dataset_to_file(saveName="dataSets/four_notes__use_octave.npz",  num_notes=4, use_octave=True,  songPath="./songs")
 
 def trainModels(dataSet):
     # Get dataset, split into training set and test set
     X, y0, y1, y2 = util.get_data_set(dataSet)
 
     X_train, X_test, y0_train, y0_test, y1_train, y1_test, y2_train, y2_test \
-        = train_test_split(X, y0, y1, y2, test_size=.15)
+        = train_test_split(X, y0, y1, y2, test_size=.15, random_state=12345)
 
     X0_train = X_train
     X0_test  = X_test
@@ -38,7 +38,7 @@ def trainModels(dataSet):
     X2_test  = X_test
 
 
-    
+    '''
     # Linear regression for delay
     print("\nTraining linear regression for delay.")
     linReg0 = LinearRegression(fit_intercept=True)
@@ -61,7 +61,7 @@ def trainModels(dataSet):
     print("Test set rms error:     %f" % np.sqrt(mean_squared_error(y1_test,  util.relu(linReg1.predict(X1_test )))))
     print("Label mean:  %f" % np.mean(y1_train))
     print("Label stdev: %f" % np.std( y1_train))
-    '''
+    
     # Multiclass logistic regression for pitch
     print("\nTraining logistic regression for pitch.")
     y2_train_i = util.one_hot_to_integer(y2_train)
@@ -73,21 +73,21 @@ def trainModels(dataSet):
     print("Training set accuracy: %f" % logReg2.score(X2_train, y2_train_i))
     print("Test set accuracy:     %f" % logReg2.score(X2_test, y2_test_i))
     util.plot_confusion_matrix(save_name="confusion_logistic.png", y_true=y2_test_i, y_pred=logReg2.predict(X2_test), normalize=False)
-    
+    '''
     
     # Neural net for delay
     for nneurons in [20, 50, 100]:
-        for nhiddenlayers in [2, 3, 4]:
-            print("\n\n\n", nneurons, "Neurons per Hidden Layer:")
-            print(nhiddenlayers, "Hidden Layers:")
+        for nlayers in [2, 3, 4]:
+            print("\n\n\n", nneurons, "Neurons per Layer:")
+            print(nlayers, "Layers:")
             print("\nTraining neural net for delay.")
             nn0 = Sequential()
-            for _ in range(nhiddenlayers):
+            for _ in range(nlayers):
                 nn0.add(Dense(nneurons, activation='relu'))
             nn0.add(Dense(1,  activation='relu'))
             nn0.compile(optimizer='adam', loss='mse', metrics=['mse'])
             hnn0 = nn0.fit(X0_train, y0_train, epochs=300, batch_size=5000, verbose=2)
-            nn0.save('nnModels/nn0/%d_neurons__%d_layers.h5' % (nneurons, nhiddenlayers))
+            nn0.save('nnModels/nn0/%d_neurons__%d_layers.h5' % (nneurons, nlayers))
             
             print("Done training.")
 
@@ -98,19 +98,21 @@ def trainModels(dataSet):
 
             #print("Training set rms error: %f" % np.sqrt(nn0.evaluate(x=X0_train, y=y0_train, verbose=0)[1])) # this is slow
             print("Test set rms error:     %f" % np.sqrt(nn0.evaluate(x=X0_test,  y=y0_test,  verbose=0)[1]))
-            plt.savefig('figs/nn0/%d_neurons__%d_layers.png' % (nneurons, nhiddenlayers))
+            plt.savefig('figs/nn0/%d_neurons__%d_layers.png' % (nneurons, nlayers))
     
     # Neural net for duration
     for nneurons in [20, 50, 100]:
-        for nhiddenlayers in [2, 3, 4]:
+        for nlayers in [2, 3, 4]:
+            print("\n\n\n", nneurons, "Neurons per Layer:")
+            print(nlayers, "Layers:")
             print("\nTraining neural net for duration.")
             nn1 = Sequential()
-            for _ in range(nhiddenlayers):
+            for _ in range(nlayers):
                 nn1.add(Dense(nneurons, activation='relu'))
             nn1.compile(optimizer='adam', loss='mse', metrics=['mse'])
             hnn1 = nn1.fit(X1_train, y1_train, epochs=300, batch_size=5000, verbose=2)
             print("Done training.")
-            nn1.save('nnModels/nn1/%d_neurons__%d_layers.h5' % (nneurons, nhiddenlayers))
+            nn1.save('nnModels/nn1/%d_neurons__%d_layers.h5' % (nneurons, nlayers))
 
             _, ax1 = plt.subplots()
             ax1.plot(np.sqrt(hnn1.history['loss']), color='tab:red')
@@ -119,9 +121,9 @@ def trainModels(dataSet):
 
             #print("Training set rms error: %f" % np.sqrt(nn1.evaluate(x=X1_train, y=y1_train, verbose=0)[1])) # this is slow
             print("Test set rms error:     %f" % np.sqrt(nn1.evaluate(x=X1_test,  y=y1_test,  verbose=0)[1]))
-            plt.savefig('figs/nn1/%d_neurons__%d_layers.png' % (nneurons, nhiddenlayers))
+            plt.savefig('figs/nn1/%d_neurons__%d_layers.png' % (nneurons, nlayers))
     
-    
+    '''
     # Neural net for pitch
     for nneurons in [20, 50, 100]:
         for nhiddenlayers in [2, 3, 4]:
@@ -160,6 +162,7 @@ def trainModels(dataSet):
         y_pred=util.one_hot_to_integer(nn2.predict(X2_test)),
         normalize=False)
     '''
+
 def main():
     # collectDatasets()
     trainModels("dataSets/four_notes__no_octave.npz")
