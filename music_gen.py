83e25ccb64ba5278b3a9fe1eab3a38a4b8a5c6db
Jacob Meisel <47841665+jacobmeisel@users.noreply.github.com>
2019-12-08 23:29:27 -0800

    skeleton part 2

--- music_gen.py
+++ music_gen.py
@@ -23,17 +23,17 @@ class LinearPredictor:
 		return delay, duration, pitch
 
 
-def generate_song_notes(num_notes, useNN=True, song_length=300, num_throwaway_notes=20, use_octave=False):
+def generate_song_notes(num_notes, use_NN, predictor, song_length=300, num_throwaway_notes=20, use_octave=False):
 	delays    = [] # list of integers
 	durations = [] # list of integers
 	pitches   = [] # list of one-hot numpy arrays
 
-	if useNN:
+	x = initialize_features(num_notes, use_octave)
+
+	if use_NN:
 		predictor = NNPredictor(num_notes, use_octave)
 	else:
 		predictor = LinearPredictor(num_notes, use_octave)
-
-	x = initialize_features(num_notes, use_octave)
 
 	for _ in range(num_throwaway_notes):
 		delay, duration, pitch  = predictor.predict(x)
@@ -50,17 +50,63 @@ def generate_song_notes(num_notes, useNN=True, song_length=300, num_throwaway_no
 	return delays, durations, pitches
 
 
-
-# Ben do this function:
 def initialize_features(num_notes, use_octave):
 	x = None
 	return x
 
 
-
 # Ben do this function:
 def update_features(x, num_notes, use_octave, new_delay, new_duration, new_pitch):
 	'''
-	Remove oldest note from x and add new_delay, new_duration, new_pitch (in one-hot form) to x
+	Remove oldest note from x and add new note to x
 	'''
 	return x
+
+
+# Ben do this function
+def generate_midi_file(midi_file_name, num_notes, delays, durations, pitches, use_octave):
+	# delays    is a list of integers
+	# durations is a list of integers
+	# pitches   is a list of one-hot numpy arrays
+	
+	# save to midi_file_name
+
+
+
+def main(use_NN):
+	### Parameters ###
+	num_notes = 4
+	use_octave = False
+	use_NN = True
+	song_length = 300
+	num_throwaway_notes = 20
+	midi_file_name = "generatedSongs/our_first_song.midi"
+
+	### Compose a song! ###
+	delays, durations, pitches = generate_song_notes(num_notes, use_NN, predictor, song_length, num_throwaway_notes, use_octave)
+	generate_midi_file(midi_file_name, num_notes, delays, durations, pitches, use_octave)
+
+if __name__ == '__main__':
+	main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
